c4a60f286937cb102a64de6621195fb3e0ea843d
Ihar Semashka <6399815@mail.ru>
2021-03-24 14:06:22 +0300

    feat: start test what-season and carbon

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if(!date) return 'Unable to determine the time of year!';
+ 
+  const  month = date.getMonth();
+ 
+  const season ='';
+
+  switch (month) {
+    case 0:
+    case 1:
+    case 11:
+    season = 'winter';
+    break;
+
+    case 2:
+    case 3:
+    case 4:
+    season = 'spring'  ;
+    break;
+
+    case 5:
+    case 6:
+    case 7:
+    season = 'summer';
+    break;
+
+    case 8:
+    case 9:
+    case 10:
+    season = 'autumn';
+    break; 
+  }
+    return season;
 };
